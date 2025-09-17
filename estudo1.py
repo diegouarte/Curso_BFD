d37a3239d236149b842a2aee9057496a671f70bd
diegouarte <xdiego.duarte@gmail.com>
2025-09-17 15:22:43 -0300

    first commit

---
+++ estudo1.py
@@ -0,0 +1,13 @@
+# str = textos
+# int = numeros inteiros
+# float = numeros flutuantes 
+# boolean = vdd ou falso ou 0 ou 1
+# round = arredondar 
+# mood = % resto que sobra
+# contas basicas = * / - +
+# print = imprimir, usar aspas
+# floor division  = // resto inteiro
+# if, elif, else = se, ou, se nao
+    # sempre usar :
+# variavel = definir guardar
+    #para atribuir usar =
